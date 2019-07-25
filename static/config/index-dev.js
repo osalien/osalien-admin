5c0bd3eafa1457e0c5edb6238e18df7a855483b2
yuanzhao <1578851175@qq.com>
2019-07-25 17:02:56 +0800

    Update https 8080

--- static/config/index-dev.js
+++ static/config/index-dev.js
@@ -5,5 +5,5 @@
   window.SITE_CONFIG = {}
   // api接口请求地址
   // window.SITE_CONFIG['baseUrl'] = 'http://182.254.171.61:8080/dbBlog'
-  window.SITE_CONFIG['baseUrl'] = 'http://47.102.155.178:8080/dbblog'
+  window.SITE_CONFIG['baseUrl'] = 'https://osb.osalien.com/dbblog'
 })()
--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -4,5 +4,5 @@
 ;(function () {
   window.SITE_CONFIG = {}
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://47.102.155.178:8080/dbblog/'
+  window.SITE_CONFIG['baseUrl'] = 'https://osb.osalien.com/dbblog/'
 })()
